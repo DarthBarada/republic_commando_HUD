a383c9f694458752e3598cf87a1b00287dfdc0b9
DarthBarada <DarthBarada@gmail.com>
2020-04-23 12:53:59 +0300

    Added via upload

--- rc_hud/cfgFunctions.hpp
+++ rc_hud/cfgFunctions.hpp
@@ -6,11 +6,11 @@ class CfgFunctions
 					{
 						class IFF_system // Система свой-чужой для Republic Commando
 							{
-								file = "RC_HUD\data\scripts\IFF_system.sqf";
+								file = "RC_HUD\functions\fnc_IFF_system.sqf";
 							};
 						class startup
 							{ 
-								file = "RC_HUD\data\scripts\startup.sqf";
+								file = "RC_HUD\functions\fnc_startup.sqf";
 							};
 					};
 			};
